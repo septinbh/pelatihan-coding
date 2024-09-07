a512d52568b330185c27c0a54281e5b7f386f89e
septinbh <septiananaibaho130@gmail.com>
2024-09-07 11:01:11 +0700

    Add files via upload

---
+++ pelatihan coding/day1.py
@@ -0,0 +1,37 @@
+a = "apel" # "a" adalah variabel dan "a" adalah data 
+print(a)
+
+buah = "jeruk" # ini adalah tipedata 'sting'
+harga = "15000" # ini adalah tipedata 'integer'
+berat = 1.5 # ini adalah tipedata 'float'
+beli = True # ini adalah tipedata 'boolean'
+
+print (buah)
+print (harga)
+print (berat)
+print (beli)
+
+# format string 
+nama = 'siti'
+umur = 16
+alamat = ' Jl. buntu'
+tinggi = 1.75
+
+print(f"nama saya {nama}. Saya berusia {umur}tahun, dan tinggi saya {tinggi} meter. saya tinggal di {alamat}.")
+
+# input
+nama = input ("siapa nama kamu?")
+print (f"Halo, {nama}")
+
+umur = int(input("umur kamu berapa?"))
+print (f"{umur}tahun")
+
+print("Halo, perkenalkan nama aku Bot!")
+print(" Halo, Bot!")
+nama = input ("Nama, kamu siapa?")
+print(f"Namaku {nama}")
+print(f"Halo,{nama}. Salam kenal!")
+print(f"Kamu sibuk apa sekarang, {nama}?")
+kegiatan = input ("kesibukanmu?")
+print(f"aku lagi sibuk {kegiatan} sekarang.")
+print(f"oke, semoga {kegiatan} berhasil.")
